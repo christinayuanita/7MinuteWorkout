3bd47f1a98f5c2f479c311f53980c93479d2e1e7
Christina Yuanita <christinayuanita380@gmail.com>
2021-05-10 09:59:48 +0700

    add exercise history to database

---
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -0,0 +1,38 @@
+package com.example.a7minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_b_m_i.*
+import kotlinx.android.synthetic.main.activity_history.*
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        setSupportActionBar(toolbar_history_activity)
+
+        val actionBar = supportActionBar
+
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.title = "HISTORY"
+        }
+
+        toolbar_history_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        getAllCompletedDates()
+    }
+
+    private fun getAllCompletedDates(){
+        val dbHandler = SqliteOpenHelper(this, null)
+        val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
+
+        for(i in allCompletedDatesList){
+            Log.i("DATE: ", i)
+        }
+    }
+}
